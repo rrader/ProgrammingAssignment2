c61e3ba0d736e7a07dae0938e8d6d0dcb0fa0954
Roman Rader <roman.rader@gmail.com>
2014-05-19 19:51:00 +0300

    Solution done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Custom matrix "object" with setter, getter and setter/getter for
+## inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inversedMatrix <- NULL
+    set <- function(m) {
+        x <<- m
+        inversedMatrix <<- NULL
+    }
+    get <- function() x
+    setInversed <- function(inversedMatrix) {
+        inversedMatrix <<- inversedMatrix
+    }
+    getInversed <- function() inversedMatrix
+    
+    list(set=set, get=get, setInversed=setInversed, getInversed=getInversed)
 }
 
 
-## Write a short comment describing this function
+## Find inverse matrix just once. Cache solution so we don't recalculate it
+## each time function called
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if (!is.null(x$getInversed())) {
+        return(x$getInversed())
+    }
+    inversedMatrix <- solve(x$get())
+    x$setInversed(inversedMatrix)
+    return(inversedMatrix)
 }
